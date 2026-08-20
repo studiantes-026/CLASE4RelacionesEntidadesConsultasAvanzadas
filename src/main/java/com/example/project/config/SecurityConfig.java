03fa67a5fade36070aadf7c2171df9cbc61d4a3b
carlosjuliogarciaorjuela <cjgarcia655@soy.sena.edu.co>
2026-08-19 21:31:21 -0500

    feat: atuh basic security

---
+++ src/main/java/com/example/project/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+}
